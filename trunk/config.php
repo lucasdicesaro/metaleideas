f5a742636ad68c7f9c1aff84e7687c7d42354643
lucasdicesaro <lucasdicesaro@52e5322d-5f86-31d2-972b-0d9439b7cf11>
2011-06-30 04:18:53 +0000

    datos

--- trunk/config.php
+++ trunk/config.php
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 $host="localhost";
-$dbname="metaleideasDB";
-$user="metaleideasuser";
-$password="metaleideasuser";
+$dbname="******";
+$user="******";
+$password="******";
 ?>
