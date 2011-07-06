951c0dbd7311850d300de6a068c07570fdd39c4d
lucasdicesaro <lucasdicesaro@52e5322d-5f86-31d2-972b-0d9439b7cf11>
2011-07-06 05:36:55 +0000

    Fix lupita

--- trunk/categoria_select_encoded.php
+++ trunk/categoria_select_encoded.php
@@ -5,6 +5,6 @@
 	mysql_select_db($dbname) or die ('Cant select Database');
 	$result = mysql_query("SELECT * FROM `categorias` ORDER BY `orden` ASC") or die(mysql_error());
 	while($row = mysql_fetch_array($result)) {
-		echo utf8_encode('<li class="ui-state-default ui-corner-top ui-corner-bottom" id="categoria_' . $row['categoria_id'] . '"><span class="ui-icon ui-icon-arrowthick-2-n-s fl_left list-icon"></span>' . $row['nombre'] . '<span onclick="borrar(' . $row['categoria_id'] . ')" class="cursor-pointer ui-icon ui-icon-close fl_right"></span></li>');																			
+		echo utf8_encode('<li class="ui-state-default ui-corner-top ui-corner-bottom" id="categoria_' . $row['categoria_id'] . '"><span class="ui-icon ui-icon-arrowthick-2-n-s fl_left list-icon"></span>' . $row['nombre'] . '<span onclick="borrar(' . $row['categoria_id'] . ')" class="cursor-pointer ui-icon ui-icon-close fl_right"></span><span onclick="buscarproductosxcategoria(' . $row['categoria_id'] . ')" class="cursor-pointer ui-icon ui-icon-search fl_right" title="Ver fotos de la categoria"></span></li>');																			
 	}
 ?>
