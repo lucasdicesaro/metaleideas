fbcc7c852f327a713a2d0cee59b13e6a60071c01
lucasdicesaro <lucasdicesaro@52e5322d-5f86-31d2-972b-0d9439b7cf11>
2011-07-06 04:19:46 +0000

    Ordenamiento de categorias. Eliminacion de categorias. El ABM de categorias es con llamadas Ajax y POST.

--- categoria_save.php
+++ categoria_save.php
@@ -3,26 +3,20 @@
 <head>
 <?
     include 'config.php';
-    $resultado='false';
-    $link = mysql_connect($host, $user, $password);
-    if(!$link)
-	{
-		echo "<h2>Error de conexi&oacute;n</h2>";
-		die();
-	}
-    mysql_select_db($dbname);
+	// Connecting to Database
+	mysql_connect ($host, $user, $password) or die ('Cant Connect to MySQL');
+	// Selecting Database
+	mysql_select_db($dbname) or die ('Cant select Database');
 
 	$query="SELECT MAX(orden)+1 FROM categorias";
-	$max_order=mysql_query ($query, $link);
+	$max_order = mysql_query ($query);
 	$max_order = mysql_fetch_array($max_order, MYSQL_BOTH);
 	$next = $max_order[0];
 	if($_REQUEST['nombre']!="")
 	{
 		$nom=htmlentities($_REQUEST['nombre']);
 		mysql_query("INSERT INTO categorias (nombre, orden) VALUES ('$nom','$next');");
-        $resultado='true';
 	}
-    mysql_close($link);
 ?>
 <meta http-equiv="REFRESH" content="0;url=categoria_consulta.php?r=<?echo($resultado)?>">
 </head>
