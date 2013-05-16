3db5bdfdd596f10d112bad53355bdcee666dec0e
lucasdicesaro <lucasdicesaro@52e5322d-5f86-31d2-972b-0d9439b7cf11>
2013-05-16 02:55:20 +0000

    se cambia el mail.

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
-<span style="margin:0 35px 0 0; align:left"><?php if ($spanish) { echo "Tel&eacute;fono"; } else { echo "Phone number"; } ?>: +54 11 4467 8062 - Email: info@metaleideas.com.ar</span>
+<span style="margin:0 35px 0 0; align:left"><?php if ($spanish) { echo "Tel&eacute;fono"; } else { echo "Phone number"; } ?>: +54 11 4467 8062 - Email: metaleideas@gmail.com</span>
 <span style="align:right"><a target="_blank" href="http://www.kinetia.com.ar/">Kinetia</a> &reg; 2009 - <?php if ($spanish) { echo "Todos los derechos reservados"; } else { echo "All rights reserved"; } ?></span>
